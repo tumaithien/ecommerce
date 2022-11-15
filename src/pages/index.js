0796f9564c652f3dede9197a2d108ecee48ffe8d
Tu Mai Thien <tumaithien@gmail.com>
2022-11-15 16:24:27 +0700

    code update search and category

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import About from "./AboutPage";
+import Checkout from "./CheckoutPage";
+import Products from "./ProductsPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  SingleProduct,
+  Cart,
+  About,
+  Checkout,
+  Products,
+  Error,
+  PrivateRoute,
+};
---
+++ src/reducers/filter_reducer.js
@@ -0,0 +1,69 @@
+import {
+  LOAD_PRODUCTS,
+  SET_LISTVIEW,
+  SET_GRIDVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from "../actions";
+
+const filter_reducer = (state, action) => {
+  if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload],
+      filters: {
+        ...state.filters,
+        max_price: maxPrice,
+        price: maxPrice,
+      },
+    };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let tempProduct = [...filtered_products];
+    if (sort === "price-lowest") {
+      tempProduct = tempProduct.sort((a, b) => a.price - b.price);
+    }
+    if (sort === "price-highest") {
+      tempProduct = tempProduct.sort((a, b) => b.price - a.price);
+    }
+    if (sort === "name-a") {
+      tempProduct = tempProduct.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+    if (sort === "name-z") {
+      tempProduct = tempProduct.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+    }
+    return { ...state, filtered_products: tempProduct };
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    console.log("filtering");
+    return { ...state };
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
